950e0f504674b7178246fd564b4afce6d8b78403
Dmitry Popov <d.popov@altgraphic.com>
2015-05-03 12:32:11 +0300

    Initial commit

--- config/module.config.php
+++ config/module.config.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * CoolMS2 Address module (http://www.coolms.com/)
+ * CoolMS2 Address Module (http://www.coolms.com/)
  *
- * @link      http://github.com/coolms/CmsAddress for the canonical source repository
+ * @link      http://github.com/coolms/address for the canonical source repository
  * @copyright Copyright (c) 2006-2015 Altgraphic, ALC (http://www.altgraphic.com)
  * @license   http://www.coolms.com/license/new-bsd New BSD License
  * @author    Dmitry Popov <d.popov@altgraphic.com>
@@ -58,48 +58,6 @@ return [
     'view_manager' => [
         'template_path_stack' => [
             __NAMESPACE__ => __DIR__ . '/../view',
-        ],
-    ],
-    'doctrine' => [
-        'driver' => [
-            __NAMESPACE__ . 'Driver' => [
-                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
-                'cache' => 'array',
-                'paths' => __DIR__ . '/../src/' . __NAMESPACE__ . '/Entity',
-            ],
-            'orm_default' => [
-                'drivers' => [
-                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . 'Driver',
-                ],
-            ],
-        ],
-        'entity_resolver' => [
-            'orm_default' => [
-                'resolvers' => [
-                    'CmsAddress\Mapping\IndividualAddressInterface' => 'CmsAddress\Entity\IndividualAddress',
-                    'CmsAddress\Mapping\LegalAddressInterface'      => 'CmsAddress\Entity\LegalAddress',
-                    'CmsAddress\Mapping\PostalAddressInterface'     => 'CmsAddress\Entity\PostalAddress',
-                    'CmsAddress\Mapping\LocalityInterface'          => 'CmsGeo\Entity\Subdivision\Locality',
-                ],
-            ],
-        ],
-        'eventmanager' => [
-            'orm_default' => [
-                'subscribers' => [
-                    'CmsAddress\AddressableSubscriber' => 'CmsAddress\Mapping\AddressableSubscriber',
-                ],
-            ],
-        ],
-        'discriminator_map' => [
-            'orm_default' => [
-                'maps' => [
-                    'CmsAddress\Entity\AbstractAddress' => [
-                        'CmsAddressIndividualAddress'   => 'CmsAddress\Entity\IndividualAddress',
-                        'CmsAddressLegalAddress'        => 'CmsAddress\Entity\LegalAddress',
-                        'CmsAddressPostalAddress'       => 'CmsAddress\Entity\PostalAddress',
-                    ],
-                ],
-            ],
         ],
     ],
 ];
